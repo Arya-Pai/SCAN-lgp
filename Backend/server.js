fc2833aac369790a7fe0421bb28aa8c672751577
Chandan U <chandanucodeid04@gmail.com>
2025-06-15 00:58:35 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 3000;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cors({
-  origin: ['http://localhost:5173','https://scan-lgp-chandu12304s-projects.vercel.app'],
+  origin: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true
 }));
